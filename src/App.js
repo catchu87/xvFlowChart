3e294f39b0d16326fcc6781b6a7cca348e22b813
catchu87 <catchu87@gmail.com>
2019-03-13 22:53:28 +0900

    Test Commit in vscode

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WorkHistory from "./log/workHistory";
 import Box from "./tool/box";
-
+/// this is testing 
 class App extends Component {
   render() {
     return (
---
+++ src/log/workHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class Work_history extends Component{
+    render(){
+        return (
+            <div>
+             
+            </div>
+        );
+    }
+};
+export default Work_history;
