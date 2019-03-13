f206fca33e47648f627d6ad7667dd7b9ca11cff8
catchu87 <catchu87@gmail.com>
2019-03-13 22:49:19 +0900

    Test in vscode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import WorkHistory from "./log/workHistory";
 import Box from "./tool/box";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        여기는 App.js  입니다.
+        <WorkHistory />
         <Box />
       </div>
     );
